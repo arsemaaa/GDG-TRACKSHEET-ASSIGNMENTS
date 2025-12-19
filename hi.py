77f69491a8628a9028ef3d5be9c272672097b5f2
arsemaaa <arsema.yeshidnber.2@gmail.com>
2025-12-19 21:22:42 +0300

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,1 +1,11 @@
-hi
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result = ""
+
+        for i in range(len(strs[0])):
+            for c in strs:
+                if i == len(c) or c[i] != strs[0][i]:
+                    return result
+            result += strs[0][i]
+        return result
+        
